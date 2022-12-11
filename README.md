969c7bbb024bae8eb97dbf12c3beefa79317255b
shani kupiec <shani.kupiec@gmail.com>
2022-12-11 16:58:33 +0200

    my first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# About-Me-React
+project number 1 in scrimba react course
+
+<p><a href="https://silver-chimera-ebe747.netlify.app/" target="blank">link to view page</a> </p>
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <main className="app">
+      <Header />
+      App
+    </main>
+  )
+}
+
+export default App
