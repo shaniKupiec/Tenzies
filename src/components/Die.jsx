79ac1d2d2ac99916d8138f558b02003f9f137629
shani kupiec <shani.kupiec@gmail.com>
2022-12-20 14:09:17 +0200

    adding toggle possible

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,6 +1,8 @@
 function Die(props) {  
+    let className = "die"
+    className += props.isHeld ? ' held' : ''
     return (
-      <div className="die">{props.value}</div>
+      <div className={className} onClick={props.toggleHold}>{props.value}</div>
     )
   }
   
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,26 +1,62 @@
+import { useState } from "react";
 import Die from "./Die";
 
 function Main() {
-  // let dieSection;
-  // for (let i = 0; i < 10; i++) {
-  //   dieSection += <Die value={i} />;
-  // }
+  const [dice, setDice] = useState(allNewDice());
 
-  function allNewDice(){
-    let array = []
+  function allNewDice() {
+    let array = [];
     for (let index = 0; index < 10; index++) {
-      array[index] = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+      array[index] = generateNewDie();
     }
-    return array
+    return array;
   }
 
-  console.log('allNewDice()',allNewDice())
+  function roll() {
+    setDice((prevState) =>
+      prevState.map((dice) => {
+        return dice.isHeld ? dice : generateNewDie();
+      })
+    );
+  }
 
-  const dieSection = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4].map((idx) => <Die value={idx}/>);
+  function generateNewDie() {
+    return {
+      value: Math.floor(Math.random() * (6 - 1 + 1) + 1),
+      isHeld: false,
+      id: makeId(),
+    };
+  }
+
+  function toggleHold(dieId) {
+    setDice((prevState) =>
+      prevState.map((die) => {
+        return die.id === dieId ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  function makeId(length = 5) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+  }
+
+  const dieSection = dice.map((diceInfo) => <Die value={diceInfo.value} key={diceInfo.id} isHeld={diceInfo.isHeld} toggleHold={() => toggleHold(diceInfo.id)} />);
 
   return (
     <main className="main">
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <section className="die-container">{dieSection}</section>
+      <button onClick={roll} className="main__btn">
+        Roll
+      </button>
     </main>
   );
 }
